8d584eb5a01a55a49a9683d7d14875c58ac67943
Marek-Sochor <83188044+Marek-Sochor@users.noreply.github.com>
2023-04-18 08:34:49 +0200

    Add files via upload

---
+++ gamdb/films/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Movie, Director
+
+def directors(request):
+    context = {
+        'logic': True,
+        'title': "Nejoblíbenější režiséři",
+        'directors': Director.objects.all()
+    }
+    print(context)
+    return render(request, 'directors.html', context)
+
+
+def movies(request):
+    context = {
+        "movies": Movie.objects.all()
+    }
+    return render(request, 'movies.html', context)
+
+def homepage(request):
+    context = {
+        # TODO use first 10 top rated
+        "movies": Movie.objects.all()
+    }
+    return render(request, 'homepage.html', context)
+
