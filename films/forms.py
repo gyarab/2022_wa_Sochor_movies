2964eb099a36a0c2e46d6feb04b74a205c9d348f
Marek-Sochor <83188044+Marek-Sochor@users.noreply.github.com>
2023-06-13 09:07:35 +0200

    Add files via upload

---
+++ films/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    author = forms.CharField(required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    text = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 5, "class" : "form-control"}))
+    rating = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={"class" : "form-control"}))
